077015c5793cb6213aa102b8b9753e33de75b7eb
Bibek Panthi <bpanthi977@gmail.com>
2025-08-05 16:48:21 -0500

    Remove old text, rename text-lcd to text

--- widgets.lisp
+++ widgets.lisp
@@ -1,46 +1,5 @@
 (in-package #:gauthali)
 
-(defwidget text (text)
-  (:state _text ttf-text width height)
-  (:build
-   ;; Compute layout ranges
-   ;; no further children widgets
-   (let ((font (property-get :font))
-	 (text-engine (property-get :text-engine)))
-     (assert font)
-     (assert text-engine)
-
-     ;; Compute properties if cache is invalid
-     (when (or (not ttf-text)
-	       (not (string-equal _text text)))
-       (setf _text text)
-       (cffi:with-foreign-string ((str bytes) text)
-	 (setf ttf-text (sdl3-ttf:create-text text-engine font str bytes))
-	 (let ((ptr ttf-text))
-	   (trivial-garbage:finalize ttf-text (lambda () (sdl3-ttf:destroy-text ptr))))
-	 (multiple-value-bind (ret w h) (sdl3-ttf:get-text-size ttf-text)
-	   (assert-ret ret)
-	   (setf width (coerce w 'single-float))
-	   (setf height (coerce h 'single-float)))))
-
-     (layout-set :flex.x 1.0
-		 :width.max width
-		 :height.min height)
-
-     nil))
-  (:on-layout-x (x w)
-		(declare (ignore x))
-		(sdl3-ttf:set-text-wrap-width ttf-text (floor w))
-		(multiple-value-bind (ret w h) (sdl3-ttf:get-text-size ttf-text)
-		  (declare (ignore w))
-		  (assert-ret ret)
-		  (setf height (coerce h 'single-float))
-		  (layout-set :height.min height)))
-  (:render (r x y w h)
-	   (declare (ignorable r h))
-	   (sdl3-ttf:set-text-wrap-width ttf-text (floor w))
-	   (sdl3-ttf:draw-renderer-text ttf-text x y)))
-
 (defun sdl3-color (color)
   (make-instance 'sdl3:color
 		 :%r (aref color 0)
@@ -48,7 +7,7 @@
 		 :%b (aref color 2)
 		 :%a (aref color 3)))
 
-(defwidget text-lcd (text)
+(defwidget text (text)
   (:state (update t) _text font surface texture fg bg render-scale max-width min-height width height)
   (:build
    ;; Compute layout ranges
@@ -124,7 +83,7 @@
    (layout-set :flex.x :least
 	       :alignment.x :center
 	       :alignment.y :center)
-   (text-lcd name))
+   (text name))
   (:render (r x y w h)
 	   (sdl3:set-render-draw-color r 125 125 125 125)
 	   (sdl3:render-rect r (make-instance 'sdl3:frect :%h h :%w w :%y y :%x x))))
@@ -142,7 +101,7 @@
 	  refresh-time)
   (:build
    (layout-set :flex.x :least)
-   (text-lcd (format nil "~,3f" (seconds-elapsed mount-time))))
+   (text (format nil "~,3f" (seconds-elapsed mount-time))))
   (:render (r x y w h)
 	   (declare (ignore r x y w h))
 	   (if (not refresh-time)
