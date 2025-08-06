0628aa2ecca926372e74193984a25d82eec0a706
Bibek Panthi <bpanthi977@gmail.com>
2025-08-05 20:18:12 -0500

    text.lisp: Fix for zero length text

--- widgets/text.lisp
+++ widgets/text.lisp
@@ -17,13 +17,18 @@
 	   bg (property-get :bg-color)
 	   fg (property-get :fg-color)
 	   render-scale (property-get :render-scale))
-
-     (multiple-value-bind (ret w h) (sdl3-ttf:get-string-size font text 0)
-       (assert-ret ret)
-       (setf max-width (/ (coerce w 'single-float) render-scale)
-	     width max-width)
-       (setf min-height (/ (coerce h 'single-float) render-scale)
-	     height min-height)))
+     (cond ((= 0 (length _text))
+	    (setf max-width 0.0
+		  width 0.0
+		  min-height 0.0
+		  height 0.0))
+	   (t
+	    (multiple-value-bind (ret w h) (sdl3-ttf:get-string-size font text 0)
+	      (assert-ret ret)
+	      (setf max-width (/ (coerce w 'single-float) render-scale)
+		    width max-width)
+	      (setf min-height (/ (coerce h 'single-float) render-scale)
+		    height min-height)))))
 
    (layout-set :flex.x 1.0
 	       :width.max max-width
@@ -32,8 +37,9 @@
 
   (:on-layout-x (x w)
 		(declare (ignore x))
-		(when (or update
-			  (not (eql width w)))
+		(when (and (or update
+			       (not (eql width w)))
+			   (not (= 0 (length text) 0)))
 		  (setf width w)
 		  (multiple-value-bind (ret w h) (sdl3-ttf:get-string-size-wrapped font text 0 (floor (* render-scale width)))
 		    (declare (ignore w))
@@ -49,16 +55,51 @@
 	   (when update
 	     (setf update nil)
 	     (when texture (sdl3:destroy-texture texture))
-	     (setf texture (sdl3:create-texture-from-surface r surface)))
-	   (multiple-value-bind (ret sx sy) (sdl3:get-render-scale r)
-	     (assert-ret ret)
-	     (when (and (= sx sy) (not (= sx 1.0)))
+	     (when surface
+	       (setf texture (sdl3:create-texture-from-surface r surface))))
+	   (when texture
+	     (multiple-value-bind (ret sx sy) (sdl3:get-render-scale r)
+	       (assert-ret ret)
+	       (when (and (= sx sy) (not (= sx 1.0)))
 		 (sdl3:set-render-scale r 1.0 1.0))
-	     (sdl3:render-texture r texture nil (make-instance 'sdl3:frect :%x (* sx x) :%y (* sy y) :%w (* w sx) :%h (* h sy)))
-	     (when (and (= sx sy) (not (= sx 1.0)))
-	       (sdl3:set-render-scale r sx sy))))
+	       (sdl3:render-texture r texture nil (make-instance 'sdl3:frect :%x (* sx x) :%y (* sy y) :%w (* w sx) :%h (* h sy)))
+	       (when (and (= sx sy) (not (= sx 1.0)))
+		 (sdl3:set-render-scale r sx sy)))))
   (:cleanup
    (when texture
      (sdl3:destroy-texture texture))
    (when surface
      (sdl3:destroy-surface surface))))
+
+#+nil(defwidget text-entry (initial-text on-change)
+  (:state (text initial-text) (focus t) border-color (cursor (length initial-text)))
+  (:build
+   (on 'sdl3:text-input-event
+       (callback (event)
+	 (when focus
+	   (setf text (concatenate 'string text (sdl3:%text event)))
+	   (funcall on-change text)
+	   (widget-rebuild))))
+
+   (on 'sdl3:keyboard-event
+       (callback (event)
+	 (when (and focus
+		    (eql (sdl3:%key event) :backspace)
+		    (> (length text) 0))
+	   (setf text (subseq text 0 (max 0 (1- (length text)))))
+	   (funcall on-change text)
+	   (widget-rebuild))))
+
+   (layout-set :flex.x :least :flex.y :least
+	       :alignment.y :center :alignment.x :start
+	       :padding.x 1.0
+	       :padding.y 1.0)
+   (setf border-color (or (property-get :border-color)
+			  (property-get :border-color)
+			  #(0 0 0 255)))
+   (text text))
+  (:render (r x y w h)
+	   (set-render-draw-color r border-color)
+	   (sdl3:render-rect r (make-instance 'sdl3:frect :%h h :%w w :%y y :%x x))
+	   (when focus
+	     (sdl3:render-line r (+ x w )))))
