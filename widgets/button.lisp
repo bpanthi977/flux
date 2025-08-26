b1306ddd8bbd7261c05772368eb08a0e9694aea3
Bibek Panthi <bpanthi977@gmail.com>
2025-08-26 15:21:29 -0500

    highlight button on hover and press

--- widgets/button.lisp
+++ widgets/button.lisp
@@ -1,6 +1,6 @@
 (in-package #:gauthali)
 
-(defwidget pressable (child-widget on-press)
+(defwidget pressable (child-widget on-press &key on-press-down)
   (:state pressed)
   (:build
    (on sdl3:mouse-button-event
@@ -14,19 +14,39 @@
 		     (setf pressed nil)))
 	       (setf pressed nil))
 	   (unless (eql old pressed)
-	     (when pressed
+	     (when (and pressed on-press-down) (funcall on-press-down))
+	     (when (not pressed) ;; press up
 	       (funcall on-press))))))
 
    child-widget))
 
-(defwidget button (name on-press)
-  (:state pressed)
+(defwidget button (name on-press &key (highlight-on-hover t))
+  (:state pressed hovered)
   (:build
+
    (layout-set this
 	       :padding 2.0
 	       :alignment.x :center
 	       :alignment.y :center)
-   (pressable (text name) on-press))
+
+
+   (let ((widget (pressable (text name)
+			    (lambda ()
+			      (setf pressed nil)
+			      (funcall on-press))
+			    :on-press-down (lambda () (setf pressed t)))))
+
+     (when highlight-on-hover
+       (setf widget (hoverable (lambda (hover) (setf hovered hover)) widget)))
+
+     widget))
   (:render (r x y w h)
-	   (sdl3:set-render-draw-color r 125 125 125 125)
-	   (sdl3:render-rect r (make-instance 'sdl3:frect :%h h :%w w :%y y :%x x))))
+	   (cond ((and (not hovered) (not pressed))
+		  (sdl3:set-render-draw-color r 125 125 125 255)
+		  (sdl3:render-rect r (make-instance 'sdl3:frect :%h h :%w w :%y y :%x x)))
+		 (pressed
+		  (sdl3:set-render-draw-color r 0 0 0 255)
+		  (sdl3:render-fill-rect r (make-instance 'sdl3:frect :%h h :%w w :%y y :%x x)))
+		 (hovered
+		  (sdl3:set-render-draw-color r 125 125 125 255)
+		  (sdl3:render-fill-rect r (make-instance 'sdl3:frect :%h h :%w w :%y y :%x x))))))
